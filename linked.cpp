51edb3cdfaf479e162f819ce7c04415d4ec071cf
SpeedReach <37238439+SpeedReach@users.noreply.github.com>
2022-10-15 21:18:17 +0800

    linked list result

--- linked.cpp
+++ linked.cpp
@@ -1,36 +1,41 @@
 #include<iostream>
-#include <list>
+#include<list>
 #include<time.h>
 #include <math.h>
-#include "random"
-
-#define N 25;
+#include <random>
 
 using namespace std;
 
-void push(list<int>& list,long amount){
+void insert(list<short>& v,int amount){
     clock_t start = clock();
     for(long i=0;i<amount;i++){
-        list.push_back(rand()%10);
+        v.push_back(rand()%10);
     }
     clock_t end = clock();
-    cout << end-start << endl;
+    cout<< "insert took: " << end - start << endl;
 }
-void iterate(list<int>& v){
+
+void addUp(list<short>& v){
     long long total = 0;
     clock_t start = clock();
     for (const auto &item : v){
         total+=item;
     }
     clock_t end = clock();
-    cout << "total = "<<total<<endl;
-    cout << "took "<< end- start << endl;
+    cout << "addUp took: "<< end- start << "  result:" << total << endl;
 }
+void run(int k){
+    long n = pow(2,k);
+    cout << endl;
+    cout << "k = " << k<< ", n = "<<n<<endl;
+    list<short> v;
+    insert(v,n);
+    addUp(v);
+}
+
 int main(){
     srand( time(NULL) );
-    list<int> list;
-    int n = N;
-    long amount = pow(2,n);
-    push(list,amount);
-    iterate(list);
+    for(int i=15;i<=30;i++){
+        run(i);
+    }
 }
